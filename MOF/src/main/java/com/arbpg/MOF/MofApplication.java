51009f35465a46b59a01fde7bab22f2ebe5e24f4
Ahmadalmatari <42914011+Ahmadalmatari@users.noreply.github.com>
2021-05-06 14:30:45 +0300

    Add files via upload

---
+++ MOF/src/main/java/com/arbpg/MOF/MofApplication.java
@@ -0,0 +1,13 @@
+package com.arbpg.MOF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MofApplication.class, args);
+	}
+
+}
---
+++ MOF/src/test/java/com/arbpg/MOF/MofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbpg.MOF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
